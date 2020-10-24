781725a67cf63b2ff94d4596a2282fabf321300c
Chiru <maheshwarreddyperam@gmail.com>
2020-10-24 16:56:19 +0530

    firstcommit

---
+++ static/js/home.js
@@ -0,0 +1,115 @@
+$(function () {
+    $('#myTab li:last-child a').tab('show')
+})
+$(".fil > .filter").click(function(){
+    $(".fil > .filter").removeClass("active");
+    $(this).addClass("active");
+});
+function myFunction1() {
+    document.getElementById("myDIV1").style.display = "block";
+    document.getElementById("myDIV2").style.display = "none";
+    document.getElementById("myDIV3").style.display = "none";
+    document.getElementById("myDIV4").style.display = "none";
+    document.getElementById("myDIV5").style.display = "none";
+}
+function myFunction2() {
+    document.getElementById("myDIV1").style.display = "none";
+    document.getElementById("myDIV2").style.display = "block";
+    document.getElementById("myDIV3").style.display = "none";
+    document.getElementById("myDIV4").style.display = "none";
+    document.getElementById("myDIV5").style.display = "none";
+}
+function myFunction3() {
+    document.getElementById("myDIV1").style.display = "none";
+    document.getElementById("myDIV2").style.display = "none";
+    document.getElementById("myDIV3").style.display = "block";
+    document.getElementById("myDIV4").style.display = "none";
+    document.getElementById("myDIV5").style.display = "none";
+}  
+function myFunction4() {
+    document.getElementById("myDIV1").style.display = "none";
+    document.getElementById("myDIV2").style.display = "none";
+    document.getElementById("myDIV3").style.display = "none";
+    document.getElementById("myDIV4").style.display = "block";
+    document.getElementById("myDIV5").style.display = "none";
+} 
+function myFunction5() {
+    document.getElementById("myDIV1").style.display = "none";
+    document.getElementById("myDIV2").style.display = "none";
+    document.getElementById("myDIV3").style.display = "none";
+    document.getElementById("myDIV4").style.display = "none";
+    document.getElementById("myDIV5").style.display = "block";
+} 
+
+
+function myFunction11() {
+    document.getElementById("myDIV11").style.display = "block";
+    document.getElementById("myDIV22").style.display = "none";
+    document.getElementById("myDIV33").style.display = "none";
+    document.getElementById("myDIV44").style.display = "none";
+    document.getElementById("myDIV55").style.display = "none";
+}
+function myFunction22() {
+    document.getElementById("myDIV11").style.display = "none";
+    document.getElementById("myDIV22").style.display = "block";
+    document.getElementById("myDIV33").style.display = "none";
+    document.getElementById("myDIV44").style.display = "none";
+    document.getElementById("myDIV55").style.display = "none";
+}
+function myFunction33() {
+    document.getElementById("myDIV11").style.display = "none";
+    document.getElementById("myDIV22").style.display = "none";
+    document.getElementById("myDIV33").style.display = "block";
+    document.getElementById("myDIV44").style.display = "none";
+    document.getElementById("myDIV55").style.display = "none";
+}  
+function myFunction44() {
+    document.getElementById("myDIV11").style.display = "none";
+    document.getElementById("myDIV22").style.display = "none";
+    document.getElementById("myDIV33").style.display = "none";
+    document.getElementById("myDIV44").style.display = "block";
+    document.getElementById("myDIV55").style.display = "none";
+}  
+function myFunction55() {
+    document.getElementById("myDIV11").style.display = "none";
+    document.getElementById("myDIV22").style.display = "none";
+    document.getElementById("myDIV33").style.display = "none";
+    document.getElementById("myDIV44").style.display = "none";
+    document.getElementById("myDIV55").style.display = "block";
+}  
+
+function myFunction111() {
+    document.getElementById("myDIV111").style.display = "block";
+    document.getElementById("myDIV222").style.display = "none";
+    document.getElementById("myDIV333").style.display = "none";
+    document.getElementById("myDIV444").style.display = "none";
+    document.getElementById("myDIV555").style.display = "none";
+}
+function myFunction222() {
+    document.getElementById("myDIV111").style.display = "none";
+    document.getElementById("myDIV222").style.display = "block";
+    document.getElementById("myDIV333").style.display = "none";
+    document.getElementById("myDIV444").style.display = "none";
+    document.getElementById("myDIV555").style.display = "none";
+}
+function myFunction333() {
+    document.getElementById("myDIV111").style.display = "none";
+    document.getElementById("myDIV222").style.display = "none";
+    document.getElementById("myDIV333").style.display = "block";
+    document.getElementById("myDIV444").style.display = "none";
+    document.getElementById("myDIV555").style.display = "none";
+}  
+function myFunction444() {
+    document.getElementById("myDIV111").style.display = "none";
+    document.getElementById("myDIV222").style.display = "none";
+    document.getElementById("myDIV333").style.display = "none";
+    document.getElementById("myDIV444").style.display = "block";
+    document.getElementById("myDIV555").style.display = "none";
+}  
+function myFunction555() {
+    document.getElementById("myDIV111").style.display = "none";
+    document.getElementById("myDIV222").style.display = "none";
+    document.getElementById("myDIV333").style.display = "none";
+    document.getElementById("myDIV444").style.display = "none";
+    document.getElementById("myDIV555").style.display = "block";
+}  
---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+var overlay = document.getElementById("overlay");
+
+window.addEventListener('load', function() {
+    overlay.style.display = 'none';
+})
+var db = document.getElementById("db");
+
+window.addEventListener('load', function() {
+    db.style.display = 'block';
+})
